7476f73c76e21ca3264fd0689586e33a21102811
hauntarl <sameer.mungole@gmail.com>
2020-12-16 18:45:03 +0530

    slight modifications to dataset

--- 9_Functional_Programming/1_immutable_ds.py
+++ 9_Functional_Programming/1_immutable_ds.py
@@ -46,10 +46,10 @@ list data structure is not, we still can add and omit data from there.
 scientists = (
     Scientist(name='Ada Lovelace', field='math', born='1815', nobel=False),
     Scientist(name='Emmy Noether', field='math', born='1882', nobel=False),
-    Scientist(name='Ada Lovelace', field='physics', born='1867', nobel=True),
-    Scientist(name='Marie Curie', field='chemistry', born='1930', nobel=True),
-    Scientist(name='Tu Youyou', field='chemistry', born='1939', nobel=True),
-    Scientist(name='Ada Yonath', field='astronomy', born='1928', nobel=False),
+    Scientist(name='Marie Curiee', field='physics', born='1867', nobel=True),
+    Scientist(name='Tu Youyou', field='chemistry', born='1930', nobel=True),
+    Scientist(name='Ada Yonath', field='chemistry', born='1939', nobel=True),
+    Scientist(name='Vera Rubin', field='astronomy', born='1928', nobel=False),
     Scientist(name='Sally Ride', field='physics', born='1951', nobel=False),
 )
 pprint(scientists)
--- 9_Functional_Programming/2_filter.py
+++ 9_Functional_Programming/2_filter.py
@@ -10,10 +10,10 @@ Scientist = collections.namedtuple('Scientist', [
 scientists = (
     Scientist(name='Ada Lovelace', field='math', born='1815', nobel=False),
     Scientist(name='Emmy Noether', field='math', born='1882', nobel=False),
-    Scientist(name='Ada Lovelace', field='physics', born='1867', nobel=True),
-    Scientist(name='Marie Curie', field='chemistry', born='1930', nobel=True),
-    Scientist(name='Tu Youyou', field='chemistry', born='1939', nobel=True),
-    Scientist(name='Ada Yonath', field='astronomy', born='1928', nobel=False),
+    Scientist(name='Marie Curiee', field='physics', born='1867', nobel=True),
+    Scientist(name='Tu Youyou', field='chemistry', born='1930', nobel=True),
+    Scientist(name='Ada Yonath', field='chemistry', born='1939', nobel=True),
+    Scientist(name='Vera Rubin', field='astronomy', born='1928', nobel=False),
     Scientist(name='Sally Ride', field='physics', born='1951', nobel=False),
 )
 
