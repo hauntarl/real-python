d64b6c40dd18ed2184fe75ac77d07dc5fb3cf2b8
hauntarl <sameer.mungole@gmail.com>
2021-02-14 20:45:48 +0530

    refactor timed decorator

--- Algorithms/sorting/timer.py
+++ Algorithms/sorting/timer.py
@@ -1,16 +1,17 @@
-from time import perf_counter
 import functools
+
+from time import perf_counter
 
 
 def timed(func):
     """Print the runtime of the decorated function"""
     @functools.wraps(func)
-    def wrapper_timer(*args, **kwargs):
-        start_time = perf_counter()  # 1
-        result = func(*args, **kwargs)
-        end_time = perf_counter()  # 2
-        run_time = end_time - start_time
-        print(f'Finished {func.__name__!r} in {run_time:.10f} secs')
-        return result
+    def wrapper_timed(*args, **kwargs):
+        beg = perf_counter()  # 1
+        res = func(*args, **kwargs)
+        end = perf_counter()  # 2
+        run = end - beg
+        print(f'Finished {func.__name__!r} in {run:.10f} secs')
+        return res
 
-    return wrapper_timer
+    return wrapper_timed
