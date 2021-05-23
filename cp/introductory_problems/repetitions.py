ced55c6aff48ad504397d2c5ea8eaf804053fd6f
hauntarl <sameer.mungole@gmail.com>
2021-05-24 00:19:11 +0530

    cses problem #3

---
+++ cp/introductory_problems/repetitions.py
@@ -0,0 +1,37 @@
+from util import timeit
+
+
+@timeit
+def repetitions(seq: str) -> int:
+    """
+    [Easy] https://cses.fi/problemset/task/1069/
+
+    You are given a DNA sequence: a string consisting of characters A, C, G, 
+    and T. Your task is to find the longest repetition in the sequence. This is 
+    a maximum-length substring containing only one type of character.
+
+    The only input line contains a string of n characters.
+    Print one integer, the length of the longest repetition.
+    Constraints: 1≤n≤106
+
+    Example
+    Input: ATTCGGGA
+    Output: 3
+    """
+    res, cur = 0, 0
+    fir = ''
+    for ch in seq:
+        if ch == fir:
+            cur += 1
+        else:
+            res = max(res, cur)
+            fir = ch
+            cur = 1
+
+    return max(res, cur)
+
+
+if __name__ == '__main__':
+    repetitions('ATTCGGGA')
+    repetitions('ACCGGGTTTT')
+    repetitions('CTCAGGTCCG')
